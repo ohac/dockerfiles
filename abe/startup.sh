702159aebe6aca101a485a2bcc9ccf4fc81bdf3b
OHASHI Hideya <ohachige@gmail.com>
2014-08-25 08:16:11 +0900

    abe: fix

--- abe/startup.sh
+++ abe/startup.sh
@@ -1,5 +1,5 @@
 mkdir -p /mnt/datadir/sha1coin
 chown sha1coin /mnt/datadir/sha1coin
 /etc/init.d/supervisor start
-python -m Abe.abe --config /mnt/data/abe.conf
+python -m Abe.abe --config abe.conf
 bash
