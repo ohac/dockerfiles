4340aa3ed4b7a8a400049b57c57f2af0b0aab30c
OHASHI Hideya <ohachige@gmail.com>
2014-08-25 12:21:51 +0900

    abe: coding

--- abe/startup.sh
+++ abe/startup.sh
@@ -2,8 +2,11 @@ mkdir -p /mnt/datadir/sha1coin
 chown sha1coin /mnt/datadir/sha1coin
 /etc/init.d/supervisor start
 echo local abe root ident >> /etc/postgresql/9.3/main/pg_hba.conf
+if [ -a /mnt/datadir/postgresql.tar.gz ]; then
+  tar zxf /mnt/datadir/postgresql.tar.gz
+fi
 service postgresql start
 sudo -u postgres createdb abe
 sudo -u postgres createuser root
-echo python -m Abe.abe --config abe.conf
+python -m Abe.abe --config abe.conf
 bash
