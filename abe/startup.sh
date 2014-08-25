18ad84da13f1a7dc3bfdccc5aea4c2e1f504103a
OHASHI Hideya <ohachige@gmail.com>
2014-08-25 15:46:20 +0900

    abe: add sakuracoin

--- abe/startup.sh
+++ abe/startup.sh
@@ -1,5 +1,7 @@
 mkdir -p /mnt/datadir/sha1coin
-chown sha1coin /mnt/datadir/sha1coin
+mkdir -p /mnt/datadir/sakuracoin
+chown coind: /mnt/datadir/sha1coin
+chown coind: /mnt/datadir/sakuracoin
 /etc/init.d/supervisor start
 echo local abe root ident >> /etc/postgresql/9.3/main/pg_hba.conf
 if [ -a /mnt/datadir/postgresql.tar.gz ]; then
