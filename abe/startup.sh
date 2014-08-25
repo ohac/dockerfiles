e3a529e0cdd8f575bee1b688f88b9a09dec34d84
OHASHI Hideya <ohachige@gmail.com>
2014-08-25 12:05:46 +0900

    abe: postgresql

--- abe/startup.sh
+++ abe/startup.sh
@@ -1,5 +1,9 @@
 mkdir -p /mnt/datadir/sha1coin
 chown sha1coin /mnt/datadir/sha1coin
 /etc/init.d/supervisor start
-python -m Abe.abe --config abe.conf
+echo local abe root ident >> /etc/postgresql/9.3/main/pg_hba.conf
+service postgresql start
+sudo -u postgres createdb abe
+sudo -u postgres createuser root
+echo python -m Abe.abe --config abe.conf
 bash
