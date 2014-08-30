1402735066f56db047d8ea92065ed9a4d630eb06
OHASHI Hideya <ohachige@gmail.com>
2014-08-30 08:06:23 +0000

    abe: coding

--- abe/startup.sh
+++ abe/startup.sh
@@ -1,8 +1,11 @@
 mkdir -p /mnt/datadir/sha1coin
 mkdir -p /mnt/datadir/sakuracoin
+mkdir -p /mnt/datadir/sayacoin
+mkdir -p /mnt/datadir/yaycoin
 chown coind: /mnt/datadir/sha1coin
 chown coind: /mnt/datadir/sakuracoin
-/etc/init.d/supervisor start
+chown coind: /mnt/datadir/sayacoin
+chown coind: /mnt/datadir/yaycoin
 echo local abe root ident >> /etc/postgresql/9.3/main/pg_hba.conf
 if [ -a /mnt/datadir/postgresql.tar.gz ]; then
   tar zxf /mnt/datadir/postgresql.tar.gz
@@ -12,7 +15,7 @@ sudo -u postgres createdb abe
 sudo -u postgres createuser root
 service redis-server start
 /etc/init.d/nginx start
-python -m Abe.abe --config abe.conf
+/etc/init.d/supervisor start
 #cd nomp
 #nodejs init.js
 bash
