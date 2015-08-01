9ce0b6ae297cc0f248a62cc988f05b841dad9e3f
OHASHI Hideya <ohachige@gmail.com>
2015-08-01 00:20:24 +0000

    withbin: add bitzeny-cpuminer

---
+++ withbin/run-coinds.sh
@@ -0,0 +1,3 @@
+sha1coind
+sakuracoind
+tail -f /dev/null
--- withbin/run-minerd.sh
+++ withbin/run-minerd.sh
@@ -1,1 +1,6 @@
-minerd -t 2 -a sha1coin -o stratum+tcp://nomp.sighash.info:3032 -O SVNGMa87FSJUed4YCzZqfvhf7LvNSMTkVn:x -f KonoHa
+# solo
+#minerd -t 2 -a sha1coin -o stratum+tcp://nomp.sighash.info:3032 -O SVNGMa87FSJUed4YCzZqfvhf7LvNSMTkVn:x -f KonoHa
+
+# pool
+#minerd.bitzeny -a yescrypt -o stratum+tcp://pool1.xau.jp:3014 -u u.w -p p
+minerd -a sha1coin -o stratum+tcp://pool1.xau.jp:3007 -u u.w -p p
